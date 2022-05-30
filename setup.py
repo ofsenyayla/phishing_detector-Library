122e72906808cb1d45f055e1e1a31f808cc60b16
Ömer Faruk Şenyayla <ofaruksenyyla@gmail.com>
2022-05-31 00:28:44 +0300

    setup update

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='phishing_detector',
     packages=find_packages(include=['phishing_detector']),
-    version='0.1.0',
+    version='0.1.1',
     description='Library that questions whether a site is phishing or not',
-    author='ofaruk',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='ofsenyayla',
+    author_email='ofaruksenyyla@gmail.com',
     license='MIT',
     classifiers=[
         "Intended Audience :: Developers",
@@ -18,5 +24,6 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent"
     ],
+    keywords='phishing detection',
     install_requires=['requests', 'validators', 'pandas'],
 )
